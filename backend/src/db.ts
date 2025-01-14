8761288c00f7d560bfcafbae8d18b86fa6eb1490
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-01-14 21:04:56 +0530

    Step 9 - Create Bank related Schema

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,12 +1,27 @@
-import mongoose, {model, Schema } from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
+import { number } from "zod";
 
 mongoose.connect(process.env.DB_CONNECTION_STRING!);
 
 const userSchema = new Schema({
-    userName: {type: String, unique: true},
-    firstName: String,
-    lastName: String,
-    password: String
-})
+  userName: {
+    type: String,
+    unique: true
+  },
+  firstName: String,
+  lastName: String,
+  password: String,
+});
 
 export const userModel = model("User", userSchema);
+
+const accountSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: { type: number, required: true },
+});
+
+export const accountModel = model("Account", accountSchema);
