0b0c169281430642ff1dee247e230efcb61f973e
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-01-18 20:33:50 +0530

    Balance component

---
+++ forntend/src/components/Balance.tsx
@@ -0,0 +1,14 @@
+type ValueProps = {
+    value: string;
+}
+
+export const Balance = ({value}: ValueProps) => {
+    return <div className="flex ">
+        <div className="font-bold text-lg">
+        Your balance
+        </div>
+        <div className="font-semibold ml-4 text-lg">
+            Rs {value}
+        </div>
+    </div>
+}
