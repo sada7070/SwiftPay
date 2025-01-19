14b174410f228397510633a5788acc64de20a4eb
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-01-20 01:16:20 +0530

    connected FE of signin to its BE

--- forntend/src/pages/signin.tsx
+++ forntend/src/pages/signin.tsx
@@ -1,20 +1,36 @@
+import { useState } from "react"
 import { Button } from "../components/Button"
 import { ButtonWarning } from "../components/ButtonWarning"
 import { Heading } from "../components/Heading"
 import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
 export const Signin = () => {
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const navigate = useNavigate();
+
     return <div className="bg-slate-300 h-screen flex justify-center">
         <div className="flex flex-col justify-center">
             <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
                 <Heading label={"Sign In"} />
                 <SubHeading label={"Enter your information to create your account"} />
-                <InputBox label={"Email"} placeholder={"john@gmail.com"} />
-                <InputBox label={"Password"} placeholder={"1234"} />
-                <Button label={"Sign in"} onClick={() =>{
-                            
-                }} />
+                <InputBox onchange={(e) => {
+                    setEmail(e.target.value)
+                }} label={"Email"} placeholder={"john@gmail.com"} />
+                <InputBox onchange={(e) => {
+                    setPassword(e.target.value)
+                }} label={"Password"} placeholder={"1234"} />
+                <Button onClick={async() =>{
+                    const response = await axios.post("http://localhost:3000/api/v1/user/signin", {
+                        email,
+                        password
+                    });
+                    localStorage.setItem("token", response.data.token);
+                    navigate("/dashboard");
+                }} label={"Sign in"} />
                 <ButtonWarning label={"Don't have an account?"} buttonText={"Sign Up"} to={"/signup"} />
             </div>
         </div>
